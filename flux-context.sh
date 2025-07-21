816b92e030b5c466f5842d38ece2bc585a2d6436
JhonnDev <jhonn.photo@gmail.com>
2025-07-21 20:23:47 +0200

    Implementing flux context provisioning script

---
+++ flux-context.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+source /venv/main/bin/activate
+COMFYUI_DIR=${WORKSPACE}/ComfyUI
+
+APT_PACKAGES=()
+PIP_PACKAGES=(
+    "torch"
+    "triton"
+)
+NODES=()
+
+WORKFLOWS=(
+   
+)
+
+UNET_MODELS=(
+)
+
+VAE_MODELS=(
+    "https://huggingface.co/Comfy-Org/Lumina_Image_2.0_Repackaged/blob/main/split_files/vae/ae.safetensors"
+
+)
+
+TEXT_ENCODERS=(
+   "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn_scaled.safetensors"
+
+)
+
+DIFFUSION_MODELS=(
+    "https://huggingface.co/Comfy-Org/flux1-kontext-dev_ComfyUI/resolve/main/split_files/diffusion_models/flux1-dev-kontext_fp8_scaled.safetensors"
+    "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors"
+)
+
+CLIP_VISION_MODELS=(
+
+)
+
+UPSCALE_MODELS=(
+   
+)
+
+function provisioning_start() {
+    provisioning_print_header
+    provisioning_get_apt_packages
+    provisioning_get_nodes
+    provisioning_get_pip_packages
+
+    workflows_dir="${COMFYUI_DIR}/user/default/workflows"
+    mkdir -p "${workflows_dir}"
+    provisioning_get_files "${workflows_dir}" "${WORKFLOWS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/vae" "${VAE_MODELS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/text_encoders" "${TEXT_ENCODERS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/diffusion_models" "${DIFFUSION_MODELS[@]}"
+    ## New implementation
+    provisioning_get_files "${COMFYUI_DIR}/models/clip_vision" "${CLIP_VISION_MODELS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/upscale_models" "${UPSCALE_MODELS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/unet" "${UNET_MODELS[@]}"
+
+    provisioning_print_end
+    echo "Installed Python packages:"
+    pip list | grep -E 'torch|triton'
+}
+
+function provisioning_get_apt_packages() {
+    if [[ -n $APT_PACKAGES ]]; then
+        sudo $APT_INSTALL ${APT_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_pip_packages() {
+    if [[ -n $PIP_PACKAGES ]]; then
+        pip install --no-cache-dir ${PIP_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_nodes() {
+    for repo in "${NODES[@]}"; do
+        dir="${repo##*/}"
+        path="${COMFYUI_DIR}/custom_nodes/${dir}"
+        requirements="${path}/requirements.txt"
+        if [[ -d $path ]]; then
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
+                echo "Updating node: ${repo}"
+                ( cd "$path" && git pull )
+                [[ -e $requirements ]] && pip install --no-cache-dir -r "$requirements"
+            fi
+        else
+            echo "Downloading node: ${repo}"
+            git clone "${repo}" "${path}" --recursive
+            [[ -e $requirements ]] && pip install --no-cache-dir -r "$requirements"
+        fi
+    done
+}
+
+function provisioning_get_files() {
+    [[ -z $2 ]] && return 1
+    dir="$1"
+    mkdir -p "$dir"
+    shift
+    arr=("$@")
+    echo "Downloading ${#arr[@]} file(s) to $dir..."
+    for url in "${arr[@]}"; do
+        echo "Downloading: $url"
+        provisioning_download "$url" "$dir"
+        echo
+    done
+}
+
+function provisioning_print_header() {
+    echo -e "\\n##############################################"
+    echo -e "#          Provisioning container            #"
+    echo -e "#         This will take some time           #"
+    echo -e "# Your container will be ready on completion #"
+    echo -e "##############################################\\n"
+}
+
+function provisioning_print_end() {
+    echo -e "\\nProvisioning complete: Application will start now\\n"
+}
+
+function provisioning_download() {
+    if [[ -n $HF_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\\.)?huggingface\\.co(/|$|\\?) ]]; then
+        auth_token="$HF_TOKEN"
+    fi
+    if [[ -n $auth_token ]]; then
+        wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes=4M -P "$2" "$1"
+    else
+        wget -qnc --content-disposition --show-progress -e dotbytes=4M -P "$2" "$1"
+    fi
+}
+
+if [[ ! -f /.noprovisioning ]]; then
+    provisioning_start
+fi
